681314ae16c0064d05421f5b7e37eb13f550e2df
omer <omerb@codevalue.net>
2019-06-03 09:23:23 +0300

    moved node count to variable

--- aks/variables.tf
+++ aks/variables.tf
@@ -5,3 +5,8 @@ variable "client_secret" {}
 variable "kubeconfig_path" {
   description = "full path to save the kubeconfig in (e.g. /root/.kube/mycluster.yaml). make sure to add this file to KUBECONFIG (e.g. export KUBECONFIG=$KUBECONFIG:/root/.kube/mycluster.yaml) in order to add it to your list of clusters"
 }
+
+variable "node_count" {
+  description = "the number of worker nodes in the pool"
+  default = 2
+}
