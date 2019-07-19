68bb9f51fbc0088045b4fb475dcf4094bd06f080
omer <omerb@codevalue.net>
2019-07-19 11:05:07 +0300

    modified syntax

--- aks/aks.tf
+++ aks/aks.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "resource_group" {
 
 # service principal for aks
 resource "azuread_application" "aks" {
-  name = terraform.workspace-random_integer.uuid.result-aks
+  name = "${terraform.workspace}-${random_integer.uuid.result}-aks"
 }
 
 resource "azuread_service_principal" "aks" {
@@ -35,7 +35,7 @@ resource "azurerm_role_assignment" "aks-network-contributor" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name-terraform.workspace
+  name                = "${var.vnet_name}-${terraform.workspace}"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   address_space       = var.vnet_address_space
